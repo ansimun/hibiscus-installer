b05d4a73ddc3963a75f06f871e846e83e7013b8b
Andreas Siegemund <andreas.siegemund@gmx.net>
2019-10-04 19:03:03 +0200

    Add tmp directory instead of writing to /tmp

--- install-hibiscus.sh
+++ install-hibiscus.sh
@@ -9,11 +9,13 @@ if [ $EUID -ne 0 ]; then
     exit 1
 fi
 
+tmpdir=$(mktemp -d -t hibiscus-XXXXX)
+
 jameica_archivename="jameica-linux`uname -m | grep -o '64'`.zip"
-jameica_url="http://www.willuhn.de/products/jameica/releases/current/jameica/$jameica_archivename"
-jameica_temp_archive="/tmp/$jameica_archivename"
-hibiscus_url="http://www.willuhn.de/products/hibiscus/releases/current/hibiscus.zip"
-hibiscus_temp_archive="/tmp/hibiscus.zip"
+jameica_url="https://www.willuhn.de/products/jameica/releases/current/jameica/$jameica_archivename"
+jameica_temp_archive="$tmpdir/$jameica_archivename"
+hibiscus_url="https://www.willuhn.de/products/hibiscus/releases/current/hibiscus.zip"
+hibiscus_temp_archive="$tmpdir/hibiscus.zip"
 
 wget $jameica_url --output-document $jameica_temp_archive
 wget $hibiscus_url --output-document $hibiscus_temp_archive
@@ -28,7 +30,5 @@ unzip -q $jameica_temp_archive -d /opt
 echo "unzip $hibiscus_temp_archive to /opt/plugins/hibiscus"
 unzip -q $hibiscus_temp_archive -d /opt/jameica/plugins
 
-echo "remove $jameica_temp_archive"
-rm $jameica_temp_archive
-echo "remove $hibiscus_temp_archive"
-rm $hibiscus_temp_archive
+echo "cleanup $tmpdir"
+rm -rf $tmpdir
